8b811ded790d0f0d850726850280661c06b7f7af
Archisha Chatterjee <archisha.chatterjee.btech2024@sitpune.edu.in>
2025-07-16 23:37:53 -0400

    Create Exp5A.cpp

---
+++ Exp5A.cpp
@@ -0,0 +1,20 @@
+// EXP 5A : Even or odd
+// Archisha Chatterjee
+// PRN : 24070123021
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter number : ";
+    cin >> a;
+    if (a%2 == 0){
+        cout << "Number is even ";
+    }
+    else{
+        cout << "Number is odd ";
+    }
+
+    return 0;
+}
